a10b9e7f490ffe2c978863977dcca061f0f5de35
SirPatschiii <tofue2003@gmx.de>
2024-05-23 08:34:50 +0200

    Implemented main bottle functions

--- src/Bottle.java
+++ src/Bottle.java
@@ -1,3 +1,21 @@
 public class Bottle {
+    private final String name;
+    private int content;
+    private final MouthPiece mouthPiece;
+    private final String height;
+    private final String diameter;
+    private final int marginalCapacity;
+    private final String weight;
+    private boolean isFilled;
 
+    public Bottle(String name) {
+        this.name = name;
+        content = 0;
+        this.mouthPiece = MouthPiece.CARNETTE;
+        height = Configuration.INSTANCE.bottleHeight;
+        diameter = Configuration.INSTANCE.diameter;
+        marginalCapacity = Configuration.INSTANCE.marginalCapacity;
+        weight = Configuration.INSTANCE.weight;
+        isFilled = false;
+    }
 }
--- src/Configuration.java
+++ src/Configuration.java
@@ -1,3 +1,8 @@
 public enum Configuration {
+    INSTANCE;
 
+    public final String bottleHeight = "164,5 mm";
+    public final String diameter = "86 mm";
+    public final int marginalCapacity = 545;
+    public final String weight = "400 gr";
 }
