cb2b7fdfed65666d6d0db683b82cb379a6df7df0
SirPatschiii <tofue2003@gmx.de>
2024-05-23 12:22:19 +0200

    Implemented JUnit imports

--- src/ParamTest.java
+++ src/ParamTest.java
@@ -1,5 +1,11 @@
-public class ParamTest {
-    public static void main(String[] args) {
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
-    }
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class ParamTest {
+
 }
