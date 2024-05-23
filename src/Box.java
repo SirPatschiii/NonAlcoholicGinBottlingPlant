e24605017bce507a1c2b41a5d21e285f86f89fb1
SirPatschiii <tofue2003@gmx.de>
2024-05-23 09:28:22 +0200

    Implemented storage area

--- src/Box.java
+++ src/Box.java
@@ -1,2 +1,11 @@
 public class Box {
+    private Bottle[][] storageArea;
+
+    public Box() {
+        storageArea = new Bottle[3][3];
+    }
+
+    public Bottle[][] getStorageArea() {
+        return storageArea;
+    }
 }
