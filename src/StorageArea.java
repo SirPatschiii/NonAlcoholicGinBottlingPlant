12861628c1ad62dd39705cfce056a0b82fbe5b4f
SirPatschiii <tofue2003@gmx.de>
2024-05-23 10:09:32 +0200

    Implemented storage area for boxes

--- src/StorageArea.java
+++ src/StorageArea.java
@@ -1,2 +1,25 @@
 public class StorageArea {
+    private Box[] storage;
+    private final int capacity = 3;
+    private int top;
+
+    public StorageArea() {
+        storage = new Box[capacity];
+    }
+
+    public boolean isFull() {
+        return top == capacity - 1;
+    }
+
+    public void addBox(Box box) {
+        if (!isFull()) {
+            storage[++top] = box;
+        } else {
+            throw new IndexOutOfBoundsException("Storage area already full!");
+        }
+    }
+
+    public Box getBox() {
+        return storage[top];
+    }
 }
