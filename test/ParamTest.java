89ab30afa34c2beeb943fef1ec9b4a4d987dbdfe
grilldb97 <dustin.bellem@web.de>
2024-05-23 18:56:46 +0200

    Test with Mockito successfully implemented

--- test/ParamTest.java
+++ test/ParamTest.java
@@ -2,34 +2,69 @@ import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
-
-import java.util.List;
-import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class ParamTest {
 
         FillingMachine fillingMachine = mock(FillingMachine.class);
+        StorageRobot storageRobot = mock(StorageRobot.class);
+        FillingRobot fillingRobot = mock(FillingRobot.class);
+        Bottle bottle = new Bottle("test", 1);
 
         @Test
-        public void TestIncreaseContent() {
-                // fillingMachine.fillingBottle();
-                doNothing().when(fillingMachine).fillingBottle();
-                // verify(fillingMachine).fillingBottle();
-
-        }
-
-
-        public void TestDeactivate() {
-
-        }
-
+        @Order(1)
         public void TestFillingBottle() {
+                doNothing().when(fillingMachine).fillingBottle();
 
         }
 
+        @Test
+        @Order(2)
+        public void TestActivateFillingMachine() {
+                doNothing().when(fillingMachine).activate();
+        }
+
+        @Test
+        @Order(3)
+        public void TestDeactivateFillingMachine() {
+                doNothing().when(fillingMachine).deactivate();
+        }
+
+        @Test
+        @Order(4)
+        public void TestActivateStorageRobot() {
+                doNothing().when(storageRobot).activate();
+        }
+
+        @Test
+        @Order(5)
+        public void TestDeactivateStorageRobot() {
+                doNothing().when(storageRobot).deactivate();
+        }
+
+        @Test
+        @Order(6)
+        public void TestActivateFillingRobot() {
+                doNothing().when(fillingRobot).activate();
+        }
+
+        @Test
+        @Order(7)
+        public void TestDeactivateFillingRobot() {
+                doNothing().when(fillingRobot).deactivate();
+        }
+
+        @Test
+        @Order(8)
+        public void TestStoreBottleFillingRobot() {
+                doNothing().when(fillingRobot).storeBottle(bottle);
+        }
+
+        @Test
+        @Order(9)
+        public void TestCommandNewBoxFillingRobot() {
+                doNothing().when(fillingRobot).commandNewBox();
+        }
 }
 
