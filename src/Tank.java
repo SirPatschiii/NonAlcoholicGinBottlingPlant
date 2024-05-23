a17958062852a7e5992fa8fa62eda2e415ef0d72
SirPatschiii <tofue2003@gmx.de>
2024-05-23 10:13:28 +0200

    Implemented tank

--- src/Tank.java
+++ src/Tank.java
@@ -1,2 +1,11 @@
 public class Tank {
+    private int content;
+
+    public Tank(int length, int width, int height) {
+        content = length * width * height;
+    }
+
+    public void decreaseContent(int amount) {
+        content = content - amount;
+    }
 }
